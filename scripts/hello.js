db99155c85c0800adb63807b44ca3400ccb17a85
pj <pj.kyosyou@outlook.jp>
2018-08-08 15:19:48 +0900

    fix message

--- scripts/hello.js
+++ scripts/hello.js
@@ -3,6 +3,6 @@
 
 module.exports = function (robot) {
   robot.hear(/hello/i, res => {
-    res.send(`Hello,  @${res.message.user.name}`)
+    res.send(`おはよう@${res.message.user.name}おにいちゃん！`)
   })
 };
