f3c08740aec31b56950cfd84ca873c598c26e670
pj <pj.kyosyou@outlook.jp>
2018-08-08 15:36:19 +0900

    fix

--- scripts/hello.js
+++ scripts/hello.js
@@ -3,12 +3,12 @@
 
 module.exports = function (robot) {
   robot.hear(/おはよう/i, res => {
-    res.send(res.random[`おはよう@${res.message.user.name}おにいちゃん！`,`うまる…きかーーん！！`, `うまる　まとめサイトの巡回で忙しいからだまってて！`]);
+    res.send(res.random[`おはよう@${res.message.user.name}おにいちゃん！`, `うまる　まとめサイトの巡回で忙しいからだまってて！`]);
   })
   robot.hear(/こんにちは/i, res => {
-    res.send(res.random[`おはよう@${res.message.user.name}おにいちゃん！`,`うまる…きかーーん！！`, `うまる　まとめサイトの巡回で忙しいからだまってて！`]);
+    res.send(res.random[`うまる…きかーーん！！`, `うまる　まとめサイトの巡回で忙しいからだまってて！`]);
   })
   robot.hear(/こんばんわ/i, res => {
-    res.send(res.random[`おはよう@${res.message.user.name}おにいちゃん！`,`うまる…きかーーん！！`, `うまる　まとめサイトの巡回で忙しいからだまってて！`]);
+    res.send(res.random[`うまる…きかーーん！！`, `うまる　まとめサイトの巡回で忙しいからだまってて！`]);
   })
 };
