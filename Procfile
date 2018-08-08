cab5e10ce593297770934925c686255cfe30d1fc
pj <pj.kyosyou@outlook.jp>
2018-08-09 08:14:44 +0900

    fix

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: bin/hubot -a slack
+web: node src/app.js
---
+++ src/app.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var app = express();
+
+var server = app.listen(3000, function () {
+	console.log("Node.js is listening to PORT:" + server.address().port);
+});
+
+app.get("/", function (req, res, next) {
+	res.json({ message: "hello world!" });
+});
+
+app.get("/refresh", function (req, res, next) {
+	res.json({ message: "bots refreshed" });
+});
