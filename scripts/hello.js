3d1f6ab02a5738ab07e1198bbe00bc3b8bf46cd2
pj <pj.kyosyou@outlook.jp>
2018-08-08 15:39:42 +0900

    hello

--- scripts/hello.js
+++ scripts/hello.js
@@ -2,13 +2,7 @@
 //   HelloWorld
 
 module.exports = function (robot) {
-  robot.hear(/おはよう/i, res => {
+  robot.hear(/hello/i, res => {
     res.send(res.random[`おはよう@${res.message.user.name}おにいちゃん！`, `うまる　まとめサイトの巡回で忙しいからだまってて！`]);
-  })
-  robot.hear(/こんにちは/i, res => {
-    res.send(res.random[`うまる…きかーーん！！`, `うまる　まとめサイトの巡回で忙しいからだまってて！`]);
-  })
-  robot.hear(/こんばんわ/i, res => {
-    res.send(res.random[`うまる…きかーーん！！`, `うまる　まとめサイトの巡回で忙しいからだまってて！`]);
   })
 };
